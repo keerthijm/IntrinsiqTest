569204431263557dbfbe16c7066769f4a2d944f1
Keerthi Jagalur Mutt <keerthijm@gmail.com>
2016-09-20 19:09:52 +0100

    Add project files.

---
+++ StudentRegistration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentRegistration.Startup))]
+namespace StudentRegistration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
